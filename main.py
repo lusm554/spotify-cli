33d1d10f6aad694e75be4b48020db512588a005b
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-26 01:47:34 +0300

    added click & set spotify API SDK

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
-import sys
-import requests
-import base64
+import click
+from decouple import config as cfg
+from reqs import API
 
-args = sys.argv[1:]
-print(f'{args=}')
-
-# Client Credentials Flow
-token_url = 'https://accounts.spotify.com/api/token'
-# tokens from spotify app 
-client_id = ''
-client_secret = ''
-
-auth_token = 'Basic ' + base64.b64encode(f'{client_id}:{client_secret}'.encode('ascii')).decode('ascii')
-headers = {
-    #'Authorization': f'Basic {client_id}:{client_secret}',
-    'Authorization': auth_token,
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
-data = {
-    'grant_type': 'client_credentials'
-}
-res = requests.post(token_url, data=data, headers=headers)
-json = res.json()
-token = json['access_token']
-print(json)
-
-h = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {token}'
-}
-
-#res = requests.post('https://api.spotify.com/v1/me/player/previous', headers=h)
-#print(res.json())
+@click.group()
+def main():
+    '''Set API SDK'''
+    global api 
+    TOKEN = cfg('TOKEN')
+    try:
+        api = API(TOKEN)
+    except ValueError as e:
+        print('The access token expired. U need to renew your token.')
+        # return status 126 when command is found but is not executable
+        exit(126)
+    print(f'You\'re logged in like {api.user["display_name"]}')
 
 
-res = requests.get('https://api.spotify.com/v1/me/player/devices', headers=h)
-print(res.json())
+@main.command('start', help='play track')
+def start():
+    pass
+
+@main.command('stop', help='stop track')
+def stop():
+    pass
+
+@main.command('next', help='play next')
+def next():
+    pass
+
+@main.command('prev', help='play previous track')
+def prev():
+    pass
+
+if __name__ == '__main__':
+    main(prog_name='spot')
