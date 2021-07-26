73cb4876e559b9265f94d6bc68ba3c2cfa972255
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-26 22:21:34 +0300

    made executable spot in another way

--- configure.sh
+++ configure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-chmod u+x spot.sh
-ln -s main.py /usr/local/bin/ 
+chmod +x spot.sh
+ln -s $PWD/main.py /usr/local/bin/spot.py
 sudo cp spot.sh /usr/local/bin/spot
+export SPOT_TOKEN_PATH="$PWD/.env"
--- spot.sh
+++ spot.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python3 ./main.py "$@"
+realpath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+CURR_DIR=$( realpath "$0" | sed 's|\(.*\)/.*|\1|')
+python3 $CURR_DIR/spot.py "$@"
