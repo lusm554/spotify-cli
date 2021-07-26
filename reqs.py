6079fe6053195364a1af4a7b80f75575fba6fff6
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-26 17:12:32 +0300

    raise API errors & current_playing_track method

--- reqs.py
+++ reqs.py
@@ -12,9 +12,6 @@ class API:
         self.s = requests.Session()
         self.s.headers.update(self.headers)
         self.user = self.__user_current_profile__()
-        if 'error' in self.user:
-            raise ValueError(self.user['error']['message'])
-
     
     def __url__(self, api='', path=''):
         '''Helper for concating API urls'''
@@ -23,17 +20,27 @@ class API:
     def __user_current_profile__(self):
         '''Fetch current user's data'''
         r = self.s.get(self.__url__())
+        if r.status_code != 200:
+            raise ValueError(r.json())
         return r.json() 
 
     def current_playback(self):
         r = self.s.get(self.__url__('/player')) 
+        if r.status_code != 200:
+            raise ValueError(f'User\'s Current Playback {r.status_code}')
         json = r.json()
-        for k, v in json.items():
-            print(k, v) 
+        return json
+
+    def current_playing_track(self):
+        r = self.s.get(self.__url__('/player', '/currently-playing'))
+        if r.status_code != 200 or not r.headers.get('content-type') == 'application/json':
+            raise ValueError(f'User\'s Currently Playing Track {r.status_code}')
+        json = r.json()
+        return json
     
     def skip_to_next(self):
         r = self.s.post(self.__url__('/player', '/next'))
         if r.status_code != 204:
-            return r.json()
+            raise ValueError(r.json())
         return r.status_code
 
