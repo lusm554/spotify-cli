cf0ca29d50c29c02985b5149d98ddfa4ee28b002
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-27 03:07:49 +0300

    docs & PEP8 style

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from reqs import API
 import time
 import os
 
+
 @click.group()
 def main():
     '''Set API SDK'''
-    global api 
+    global api
     path_to_env = os.environ.get('SPOT_TOKEN_PATH')
     if not path_to_env:
         print('Set SPOT_TOKEN_PATH env')
         exit(1)
-    env = Config(RepositoryEnv(path_to_env)) 
+    env = Config(RepositoryEnv(path_to_env))
     TOKEN = env.get('TOKEN')
     try:
         api = API(TOKEN)
@@ -25,11 +26,15 @@ def main():
         exit(1)
     print(f'You\'re logged in like {api.user["display_name"]}')
 
+
 def __pretty__(track):
     '''Creats pretty form of track'''
     name = track['item']['name']
-    artists = ', '.join([artist['name'] for artist in track['item']['artists']])
+    artists = ', '.join(
+        [artist['name'] for artist in track['item']['artists']]
+    )
     return '  {} - {}'.format(artists, name)
+
 
 @main.command('start', help='play track')
 def start():
@@ -41,6 +46,7 @@ def start():
         exit(1)
     print('Now playing')
     print(__pretty__(track))
+
 
 @main.command('stop', help='stop track')
 def stop():
@@ -56,7 +62,7 @@ def stop():
 
 def skip(action):
     '''Helper for skip methods'''
-    _skip = api.skip_to_next if action=='next' else api.skip_to_previous
+    _skip = api.skip_to_next if action == 'next' else api.skip_to_previous
     try:
         curr = api.current_playback()
         r = _skip()
@@ -77,14 +83,17 @@ def skip(action):
     print('Now playing')
     print(__pretty__(curr))
 
+
 @main.command('next', help='play next')
 def next():
     skip('next')
-    
+
+
 @main.command('prev', help='play previous track')
 def prev():
     skip('prev')
-    
+
+
 @main.command('curr', help='what track is playing now')
 def curr():
     try:
@@ -92,9 +101,12 @@ def curr():
     except ValueError as e:
         print(e)
         exit(1)
-    print('Pausing' if 'pausing' in track['actions']['disallows'] else 'Playing now')
+    if 'pausing' in track['actions']['disallows']:
+        print('Pausing')
+    else:
+        print('Playing now')
     print(__pretty__(track))
+
 
 if __name__ == '__main__':
     main(prog_name='spot')
-
--- reqs.py
+++ reqs.py
@@ -2,6 +2,10 @@ import requests
 
 
 class API:
+    '''
+    Class that wraps Spotify REST API into python methods.
+    '''
+
     def __init__(self, token):
         self.token = token
         self.headers = {
@@ -12,7 +16,7 @@ class API:
         self.s = requests.Session()
         self.s.headers.update(self.headers)
         self.user = self.__user_current_profile__()
-    
+
     def __url__(self, api='', path=''):
         '''Helper for concating API urls'''
         return f'https://api.spotify.com/v1/me{api}{path}'
@@ -22,43 +26,57 @@ class API:
         r = self.s.get(self.__url__())
         if r.status_code != 200:
             raise ValueError(r.json())
-        return r.json() 
+        return r.json()
 
     def pause_playback(self):
+        '''Pause playback on the user’s account.'''
         r = self.s.put(self.__url__('/player', '/pause'))
         if r.status_code != 204:
             raise ValueError(f'Pause a User\'s Playback {r.status_code}')
         return r.status_code
 
     def start_resume_playback(self):
+        '''Start a new context or resume current playback
+        on the user’s active device.'''
         r = self.s.put(self.__url__('/player', '/play'))
         if r.status_code != 204:
-            raise ValueError(f'Start/Resume a User\'s Playback {r.status_code}')
+            raise ValueError(f'Start/Resume User\'s Playback {r.status_code}')
         return r.status_code
 
     def current_playback(self):
-        r = self.s.get(self.__url__('/player')) 
+        '''Get information about the user’s current playback state,
+        including track or episode, progress, and active device.'''
+        r = self.s.get(self.__url__('/player'))
         if r.status_code != 200:
             raise ValueError(f'User\'s Current Playback {r.status_code}')
         json = r.json()
         return json
 
     def current_playing_track(self):
+        '''Get the object currently being played
+        on the user’s Spotify account.'''
         r = self.s.get(self.__url__('/player', '/currently-playing'))
-        if r.status_code != 200 or not r.headers.get('content-type') == 'application/json':
-            raise ValueError(f'User\'s Currently Playing Track {r.status_code}')
+        is_json = r.headers.get('content-type') == 'application/json'
+        if r.status_code != 200 or not is_json:
+            raise ValueError(
+
+                f'User\'s Currently Playing Track {r.status_code}'
+            )
         json = r.json()
         return json
-    
+
     def skip_to_next(self):
+        '''Skips to next track in the user’s queue.'''
         r = self.s.post(self.__url__('/player', '/next'))
         if r.status_code != 204:
             raise ValueError(r.json())
         return r.status_code
-    
+
     def skip_to_previous(self):
+        '''Skips to previous track in the user’s queue.'''
         r = self.s.post(self.__url__('/player', '/previous'))
         if r.status_code != 204:
-            raise ValueError(f'Skip User\’s Playback To Previous Track {r.status_code}')
+            raise ValueError(
+                f'Skip User’s Playback To Previous Track {r.status_code}'
+            )
         return r.status_code
-
